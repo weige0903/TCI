aa9d15035089b844065cc3a5029ced0a5dbbf38d
weige0903 <46675509+weige0903@users.noreply.github.com>
2022-11-28 01:10:54 +0800

    Create First_Bus.js

---
+++ First_Bus.js
@@ -0,0 +1,30 @@
+  .mobile .First_Bus {
+  	width:100%;
+    border:0px solid black;
+    background-color: rgba(0,0,0,0.15);
+  }
+  .mobile .First_Bus td{padding: 0px; }
+  .mobile .First_Bus_title_1 { 	width:22%;  }
+  .mobile .First_Bus_title_2 { 	width:26%;  }
+
+  .mobile .First_Bus td {border:1px solid rgb(65 65 65);}
+  .mobile .First_Bus_title_1 {background-color: rgba(0,0,0,0.25);}
+  .mobile .First_Bus_title_2 {background-color: rgba(0,0,0,0.25);}
+  
+  .First_Bus td{
+    padding: 6px;
+    border:1px solid rgb(70 70 70);
+  }
+  .First_Bus {  width:80%; }
+  .First_Bus_title_1 {	width:18%;  }
+  .First_Bus_title_2 {	width:19%;  }
+  .First_Bus_title_1,.First_Bus_title_2 {    
+    //background-color: rgba(98,0,0,0.9);
+    color: rgb(255, 255, 255);
+    text-align: center;
+	line-height: 1.5;
+  }
+  
+  table{
+  border-collapse:collapse;
+  }  
